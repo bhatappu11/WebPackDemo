5d6a8c0b3585532cd5458ad143bc7d96d810defb
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-19 08:53:02 +0530

    [Arpitha]ADDED: App and index js and css files required for react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App() {
+    return (<div>
+        <h2>Welcome to webpack demo</h2>
+        <span>------oxoxox------</span>
+        <h3>Date : {new Date().toDateString()}</h3>
+    </div>)
+}
+
+export default App
